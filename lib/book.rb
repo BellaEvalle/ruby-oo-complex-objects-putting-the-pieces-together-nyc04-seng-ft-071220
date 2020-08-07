49367a0bffe50fe2d4e2a7371d21c7f6c0729efc
Bella Evalle <bellaevalle@yahoo.com>
2020-08-07 13:23:25 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,7 +16,13 @@ class Book
    @author
   end 
   
-  
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+    
 end
 
 Book.new("And Then There Were None")
